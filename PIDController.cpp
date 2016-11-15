6f0f6e6e8a728b799827cf07f55e13a7ca24e208
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-15 20:41:36 +0100

    *	Removed uneeded init file and implemented it in the contructors *	Added names of parameters in header file *	Removed setter and getter funtions

--- PIDController.cpp
+++ PIDController.cpp
@@ -3,29 +3,24 @@
 #include <chrono>
 
 
-PIDController::PIDController(){
-    init(0.0, 0.0, 0.0, 0.0, 0.0, std::chrono::duration<float>(std::chrono::duration_values<float>::zero()));
-}
+PIDController::PIDController():
+PIDController(
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    0.0,
+    std::chrono::duration<float>(std::chrono::duration_values<float>::zero())){}
 
-PIDController::PIDController(float p, float i, float d, float prev, float integral, std::chrono::duration<float> sampleTime){
-    init(p, i, d, prev, integral, sampleTime);
-}
+PIDController::PIDController(float p, float i, float d, float prev, float integral, std::chrono::duration<float> sampleTime):
+    Kp(p),
+    Ki(i),
+    Kd(d),
+    dT(sampleTime),
+    ePrevious(prev),
+    eIntegral(integral){}
 
 PIDController::~PIDController(){}
-
-void PIDController::init(float p, float i, float d, float prev, float integral, std::chrono::duration<float> sampleTime){
-    //Transfer constants
-    Kp = p;
-    Ki = i;
-    Kd = d;
-    //Set initial values
-    ePrevious = prev;
-    eIntegral = integral;
-    //Set sample time
-    dT =  sampleTime;
-    //Set output
-    controlOutput = 0.0;
-}
 
 float PIDController::calculateOutput(float eInput){
     //Integrating the error
@@ -43,20 +38,7 @@ float PIDController::calculateOutput(float eInput){
 float PIDController::calculateOutput(float eInput, std::chrono::duration<float> sampleTime){
     //Store the new sample time
     dT = sampleTime;
-    calculateOutput(eInput);
+    return calculateOutput(eInput);
 }
 
-inline float PIDController::getOutput() {return controlOutput;};
-
-inline void PIDController::setConstants(float p, float i, float d){
-    Kp = p;
-    Ki = i;
-    Kd = d;
-}
-
-inline std::tuple<float, float, float> PIDController::getConstants(){
-    return std::tuple<float,float,float>(Kp, Ki, Kd);
-}
-
-inline void PIDController::setSampleTime(std::chrono::duration<float> sampleTime) {dT = sampleTime;}
-inline std::chrono::duration<float> PIDController::getSampleTime() {return dT;}; 
+float PIDController::getOutput() {return controlOutput;}
--- PIDController.h
+++ PIDController.h
@@ -3,28 +3,21 @@
 #define _PIDCONTROLLER_H_
 
 #include <chrono>
-#include <tuple>
 
 class PIDController{
     public:
         PIDController();
-        PIDController(float, float, float, float, float, std::chrono::duration<float>);
+        PIDController(float p, float i, float d, float prev, float integral, std::chrono::duration<float> sampleTime);
         ~PIDController();
         
-        float calculateOutput(float);
-        float calculateOutput(float, std::chrono::duration<float>);
+        float calculateOutput(float eInput);
+        float calculateOutput(float eInput, std::chrono::duration<float> sampleTime);
         float getOutput();
         
-        void setConstants(float,float,float);
-        std::tuple<float, float, float> getConstants();
-
-        void setSampleTime(std::chrono::duration<float>);
-        std::chrono::duration<float> getSampleTime();
-        
-    private:
-        void init(float, float, float, float, float, std::chrono::duration<float>);
         float Kp, Ki, Kd; //P, I and D constants
         std::chrono::duration<float> dT; //Sampling time
+        
+    private:
         float ePrevious, eIntegral, controlOutput; //Error and output signals
 };
 #endif
