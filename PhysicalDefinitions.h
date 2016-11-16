ae5a097185118baa27399e7a9ebe6d771fdca1da
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-16 11:38:39 +0100

    Now using a Vector2 library from github

--- PhysicalDefinitions.h
+++ PhysicalDefinitions.h
@@ -1,12 +1,10 @@
 
 //Al units are in basic SI units. m, kg, s etc. unless other specified
 
-class Vector2{
-	float x, y;
-};
+#include "Vector2/Vector2.hpp"
 
 class BaseThurster{
-	Vector2 localLocation;
+	Vector2<float> localLocation;
 	float normalRotation;
 	float throttle;
 };
@@ -18,24 +16,24 @@ class AzimuthThruster: public BaseThurster{
 
 class Boat{
 public:
-	Vector2 getPosition();
-	gotoPosition(Vector2 newPosition, float speed);
+	Vector2<float> getPosition();
+	void gotoPosition(Vector2<float> newPosition, float speed);
 	AzimuthThruster azimuthThruster[2];
 private:
 	//Positional states
-	SensorCalculations sensorCalculations;
-	Vector2 currentPosition = 0; //Current position of the boat (in X,Y).
-	Vector2 currentSpeed = 0; //Current speed of the boat (in X,Y).
+	//SensorCalculations sensorCalculations;
+	Vector2<float> currentPosition = 0; //Current position of the boat (in X,Y).
+	Vector2<float> currentSpeed = 0; //Current speed of the boat (in X,Y).
 	float setpointSpeed = 0; //The maximal speed you want (user input).
-	Vector2 setpointPosition = 0; //The position the boat is heading to.
+	Vector2<float> setpointPosition = 0; //The position the boat is heading to.
 	float currentHeading = 0, setpointHeading = 0; //Heading is the amount of degrees.
 	//Physical properies
 	float mass;
 	float angularMass;
-	Vector2 directianalDamping;
+	Vector2<float> directianalDamping;
 	float angularDamping;
 };
 
 class controller{
-	*Boat boat;
+	Boat* boat;
 };
