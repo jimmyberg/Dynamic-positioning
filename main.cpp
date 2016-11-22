651d304815fc011b1a2b60efa5d2337923f8d759
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-22 16:23:38 +0100

    Updated main

--- main.cpp
+++ main.cpp
@@ -1,10 +1,24 @@
 #include <iostream>
 #include "PhysicalDefinitions.h"
-#include "PIDController.h"
+#include "BoatController.h"
+#include "Simulate.h"
 
 using namespace std;
 
 int main(void){
-
+	Boat boat;
+	SimulatedWorld simulation(&boat, 0.001);
+	BoatController boatController(&boat);
+	cout << boat.currentPosition << endl;
+	cout << boat.azimuthThruster[0].throttle << endl;
+	for (int i = 0; i < 10; ++i)
+	{
+		for (int j = 0; j < 10; ++j)
+		{
+			simulation.calculateWorldTick();
+			cout << boat.currentPosition << endl;
+		}
+		boatController.singleStep();
+	}
     return 0;
 }
