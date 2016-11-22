84ed23d836ca6f28c155907cf3b17197c6bdbaa2
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-22 16:31:20 +0100

    MAin

--- main.cpp
+++ main.cpp
@@ -9,6 +9,9 @@ int main(void){
 	Boat boat;
 	SimulatedWorld simulation(&boat, 0.001);
 	BoatController boatController(&boat);
+	boat.mass = 10;
+	boat.angularMass = 5;
+
 	cout << boat.currentPosition << endl;
 	cout << boat.azimuthThruster[0].throttle << endl;
 	for (int i = 0; i < 10; ++i)
