72be15890d4da4361595df3759cfd0d23ca638fb
Jimmy van den Berg <vandenberg.jimmy@gmail.com>
2016-11-22 15:03:04 +0100

    Made main.cpp empty

--- main.cpp
+++ main.cpp
@@ -6,9 +6,6 @@
 using namespace std;
 
 int main(void){
-	DoubleIntegral test(0.01);
-	for (int t = 0; t < 1.0; t += 0.01){
-		cout << t << ',' << test.inputVal(1) << endl; 
-	}    
-    return 0;
+
+	return 0;
 }
