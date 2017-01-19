22e2bf44eecd564e98b24bac4cb75571cc750d3a
Jesse Hoogervorst <hoogervorstjesse@gmail.com>
2017-01-19 23:13:42 +0100

    Added Doxygen lines to BoatController and PIDController

--- BoatController.h
+++ BoatController.h
@@ -5,38 +5,91 @@
 #include <thread>
 #include <chrono>
 
+
+/**
+ * @brief      Class that implements the controller of the boat
+ */
 class BoatController{
 	public:
+
+        /**
+         * @brief      Constructor
+         *
+         * @param      b     Pointer to the boat object to control
+         */
         BoatController(Boat *b);
+        
+        /**
+         * @brief      Destroys the object.
+         */
         ~BoatController();
 
-        //Start stop control thread
+        
+        /**
+         * @brief      Starts the boatController internal thread.
+         */
         void startControl();
+
+        /**
+         * @brief      Stops the boatController internal thread.
+         */
         void stopControl();
 
+        /**
+         * @brief      Determines if the boatController thread is running.
+         *
+         * @return     True if running, False otherwise.
+         */
         bool isRunning() {return running;};
 
+        /**
+         * @brief      Does a single control step.
+         */
         void singleStep();
 
-        float xSignal = 0.0;
-        float ySignal = 0.0;
-        float hSignal = 0.0;
+        float xSignal = 0.0; //!< The current control signal of the x position
+        float ySignal = 0.0; //!< The current control signal of the y position
+        float hSignal = 0.0; //!< The current control signal of the heading
 
 	private:
-        Boat *boat;
-        std::thread controlThread;
-        std::chrono::duration<float> periodTime;
-        std::chrono::time_point<std::chrono::high_resolution_clock> lastTime, currentTime;
+        Boat *boat; //!< The pointer to the boat that is controlled by this class
+        std::thread controlThread; //!< The internal thread that will control the boat periodically
+        std::chrono::duration<float> periodTime; //!< The current periodTime of the thread
+        std::chrono::time_point<std::chrono::high_resolution_clock> lastTime, currentTime; //!< Used to determine the periodTime
 
-        bool running = false;
+        bool running = false; //!< Stores if the control thread is running or not
 
         //Controllers
-        PIDController xController;
-		PIDController yController;
-		PIDController hController;
+        PIDController xController; //!< The PIDController of the x position
+	PIDController yController; //!< The PIDController of the y position
+	PIDController hController; //!< The PIDController of the heading
 
+        /**
+         * @brief      The function that runs in the boatController controlThread
+         */
         void controlFunction();
+
+        /**
+         * @brief      Converts the heading control signal to a vector that can be used to control a thruster
+         *
+         * @param[in]  hSignal  The heading signal
+         * @param[in]  left     The angle if the thruster has to turn the boat to the left
+         * @param[in]  right    The angle if the thruster has to turn the boat to the right
+         *
+         * @return     The vector for the thruster determined by the heading
+         */
         Vector2<float> getHeadingVector(float hSignal, float left, float right);
+
+        /**
+         * @brief      Used to write left, or right to the output if the input is above or below a threshold
+         *
+         * @param[in]  error      The error signal
+         * @param[in]  threshold  The threshold
+         * @param[in]  left       The left angle
+         * @param[in]  right      The right angle
+         *
+         * @return     The angle associated to the input
+         */
         float thresholding(float error, float threshold, float left, float right);
 };
 #endif
--- PIDController.h
+++ PIDController.h
@@ -4,21 +4,77 @@
 
 #include <chrono>
 
+
+/**
+ * @brief      Class implemention a PID controller
+ */
 class PIDController{
     public:
+        /**
+         * @brief      Default constructor
+         */
         PIDController();
+        /**
+         * @brief      Constructor without sampleTime
+         *
+         * @param[in]  p         Gain of the P controller
+         * @param[in]  i         Gain of the I controller
+         * @param[in]  d         Gain of the D controller
+         * @param[in]  prev      The previous value of the error signal
+         * @param[in]  integral  The starting integrated value
+         * @param[in]  iGain     The input gain
+         */
         PIDController(float p, float i, float d, float prev, float integral, float iGain);
+        
+        /**
+         * @brief      Constructor with sampleTime
+         *
+         * @param[in]  p           Gain of the P controller
+         * @param[in]  i           Gain of the I controller
+         * @param[in]  d           Gain of the D controller
+         * @param[in]  prev        The previous value of the error signal
+         * @param[in]  integral    The starting integrated value
+         * @param[in]  iGain       The input gain
+         * @param[in]  sampleTime  The sample time
+         */
         PIDController(float p, float i, float d, float prev, float integral, float iGain, std::chrono::duration<float> sampleTime);
+        
+        /**
+         * @brief      Destroys the object.
+         */
         ~PIDController();
         
+
+        /**
+         * @brief      Calculates the output.
+         *
+         * @param[in]  eInput  The error input
+         *
+         * @return     The output control signal.
+         */
         float calculateOutput(float eInput);
+        
+        /**
+         * @brief      Calculates the output.
+         *
+         * @param[in]  eInput      The error input
+         * @param[in]  sampleTime  The sample time
+         *
+         * @return     The output.
+         */
         float calculateOutput(float eInput, std::chrono::duration<float> sampleTime);
+        
+        /**
+         * @brief      Gets the last control signal output.
+         *
+         * @return     The output control signal.
+         */
         float getOutput();
         
-        float Kp, Ki, Kd, inputGain; //P, I and D constants
-        std::chrono::duration<float> dT; //Sampling time
+        float Kp, Ki, Kd, inputGain; //!<P, I and D constants, and the input gain
+        std::chrono::duration<float> dT; //!<Sampling time
         
     private:
-        float ePrevious, eIntegral, controlOutput; //Error and output signals
+        float ePrevious, eIntegral, controlOutput; //!<Previous error, integral error and output signals
 };
 #endif
